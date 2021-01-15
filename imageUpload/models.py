87e98afc6c9a6a9e178e581b6ce0a7ed49002f74
samuel-octavio-97 <samupereira2@gmail.com>
2021-01-15 21:05:49 +0000

    add Image model

--- imageUpload/models.py
+++ imageUpload/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Image(models.Model):
+    name = models.CharField(max_length=50)
+    image_file = models.ImageField(
+        upload_to='')
